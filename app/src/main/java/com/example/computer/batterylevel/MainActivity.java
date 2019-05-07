35a0717d5f162f214b995534b42600a750e4ec2c
segunfrancis <francis4dblues@gmail.com>
2019-05-07 08:48:27 +0100

    Add Toast

--- app/src/main/java/com/example/computer/batterylevel/MainActivity.java
+++ app/src/main/java/com/example/computer/batterylevel/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView batteryImage;
@@ -17,8 +18,13 @@ public class MainActivity extends AppCompatActivity {
         batteryImage = findViewById(R.id.battery_image);
     }
 
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
     public void increaseBatteryLevel(View view) {
         if (imageLevel == 6) {
+            showToast("Can't go any higher");
             return;
         } else {
             imageLevel++;
@@ -28,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void decreaseBatteryLevel(View view) {
         if (imageLevel == 0) {
+            showToast("Can't go any lower");
             return;
         } else {
             imageLevel--;
